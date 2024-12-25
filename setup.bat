ca013df2f2ae068cd1d10d392fda65d8e37d747c
Fipaan <fipaan@fipaan-pc.localdomain>
2024-12-25 09:33:35 +0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 mkdir classes\
-javac -d classes\ Charity.java Donation.java Donor.java Main.java
+javac -d classes\ Charity.java Donation.java Donor.java Person.java Company.java Main.java
 cd src/
 java -cp classes\ Main
 cd ..
