abe9dad2f8b8220a4b79d49c02774f59d0941ffd
Fipaan <okostenyy@mail.ru>
2025-02-11 22:44:46 +0500

    Rebase my project onto IntelliJ IDEA

--- Makefile
+++
@@ -1,9 +0,0 @@
-
-all: build run
-
-build:
-	mkdir -p classes/
-	javac -cp "include/*" src/* -d classes/
-
-run:
-	java -cp classes/:./include/* Main 
--- setup.bat
+++
@@ -1,5 +0,0 @@
-mkdir classes\
-javac -d classes\ Charity.java Donation.java Donor.java Person.java Company.java Main.java
-cd src/
-java -cp classes\ Main
-cd ..
