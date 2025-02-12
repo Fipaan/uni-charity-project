67cf7bc964c89478f8489db0bde32c5725449876
Fipaan <git://github.com/fipaan>
2025-02-12 08:27:24 +0500

    Provided more clear responses from server

--- src/main/java/org/example/CharityController.java
+++ src/main/java/org/example/CharityController.java
@@ -11,33 +11,46 @@ public class CharityController {
         try {
             return DatabaseLinker.get_charity(name);
         } catch (Exception e) {
-            return null;
+            return new Charity(String.format("Charity with name %s not found", name));
         }
     }
 
     @PostMapping("/post/{charity_name}/{donation_name}/donors")
-    public int addDonor(@PathVariable String charity_name, @PathVariable String donation_name, @RequestBody Donor donor) {
+    public String addDonor(@PathVariable String charity_name, @PathVariable String donation_name, @RequestBody Donor donor) {
         try {
-            return DatabaseLinker.upsert_donor(DatabaseLinker.get_donation_id(charity_name, donation_name), donor) != -1 ? 1 : 0;
+            int donor_id = DatabaseLinker.upsert_donor(DatabaseLinker.get_donation_id(charity_name, donation_name), donor);
+            return String.format("Successfully added Donor! (%d)", donor_id);
         } catch (Exception e) {
-            return -1;
+            return "Couldn't add Donor";
+        }
+    }
+
+    @DeleteMapping("/remove/{charityName}/{donationName}/donors/{donorName}")
+    public String removeDonor(@PathVariable String charityName, @PathVariable String donationName, @PathVariable String donorName) {
+        try {
+            int count = DatabaseLinker.remove_donor(charityName, donationName, donorName);
+            return count > 0 ? "Successfully deleted Donor" : "Couldn't remove Donor";
+        } catch (Exception e) {
+            return "Couldn't remove Donor";
         }
     }
 
     @PostMapping("/post/{charity_name}/donations")
-    public int addDonation(@PathVariable String charity_name, @RequestBody Donation donation) {
+    public String addDonation(@PathVariable String charity_name, @RequestBody Donation donation) {
         try {
-            return DatabaseLinker.upsert_donation(DatabaseLinker.get_charity_id(charity_name), donation) != -1 ? 1 : 0;
+            int donation_id = DatabaseLinker.upsert_donation(DatabaseLinker.get_charity_id(charity_name), donation);
+            return String.format("Successfully added Donation! (%d)", donation_id);
         } catch (Exception e) {
-            return -1;
+            return "Couldn't add Donation";
         }
     }
     @DeleteMapping("/remove/{charityName}/donations/{donationName}")
-    public int removeDonation(@PathVariable String charityName, @PathVariable String donationName) {
+    public String removeDonation(@PathVariable String charityName, @PathVariable String donationName) {
         try {
-            return DatabaseLinker.remove_donation(charityName, donationName) != -1 ? 1 : 0;
+            int count = DatabaseLinker.remove_donation(charityName, donationName);
+            return count > 0 ? "Successfully deleted Donation" : "Couldn't remove Donation";
         } catch (Exception e) {
-            return -1;
+            return "Couldn't remove Donation";
         }
     }
 }
